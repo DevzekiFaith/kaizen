33846d5f4334cd8ef87e7979713907c694d25c89
DevzekiFaith <zekipossible@gmail.com>
2024-10-13 00:30:00 +0100

    env_work

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -11,7 +11,9 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
-
+console.log("API Key:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+console.log("Auth Domain:", process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN);
+// ... log other variables as needed
 // Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
