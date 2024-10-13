5b3813a0f637bf7d31b174145e145d7fea00c30c
DevzekiFaith <zekipossible@gmail.com>
2024-10-13 10:48:41 +0100

    image_changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["kaizen-chi.vercel.app"], // Add any domains you use for hosting images.
+        domains: ["kaizen-chi.vercel.app", "https://www.pinterest.com/"], // Add any domains you use for hosting images.
         formats: ["image/avif", "image/webp"],
     },
 };
